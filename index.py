324c72262f9ac9631442f1e3afa18cbc2f349037
lewiswolf <lewiswolstanholme@gmail.com>
2022-10-27 09:41:12 +0100

    fixes to loadModel

--- index.py
+++ index.py
@@ -33,14 +33,20 @@ if __name__ == '__main__':
 	# import os
 
 	# # src
+	# from kac_drumset import loadDataset
 	# from kac_prediction.architecture import SizeOfCircularDrum
 	# from kac_prediction.pipeline import loadModel
 
 	# # load model
-	# path_to_parameters = os.path.normpath(f'{__file__}/../model/parameters')
+	# path_to_parameters = os.path.normpath(f'{__file__}/../model/parameters.model')
 	# model = loadModel(
 	# 	SizeOfCircularDrum,
 	# 	path_to_parameters,
-	# 	'https://api.wandb.ai/files/lewiswolf/kac_prediction%20(circular%20drum%20size)/n698aid7/epoch_100.model',
+	# 	'https://api.wandb.ai/files/lewiswolf/kac_prediction%20(circular%20drum%20size)/otzl85e8/epoch_188.model',
 	# )
+
+	# # test
+	# x, y = loadDataset(os.path.normpath(f'{os.path.dirname(__file__)}/data')).__getitem__(0)
+	# print(f'The predicted drum size is {model(x[-1024:].view(1, -1)).item()}')
+	# print(f'The true drum size is {y["drum_size"].item()}')
 	# exit()
--- kac_prediction/pipeline/load_model.py
+++ kac_prediction/pipeline/load_model.py
@@ -23,14 +23,19 @@ def loadModel(Model: type[torch.nn.Module], path_to_parameters: str, url: Option
 	specified url. Finally, a model is loaded and initialised.
 	'''
 
-	# download parameters using curl
-	# could change this to `pip install wget` if this doesn't work on all os
+	# assert params exist
 	if not os.path.exists(path_to_parameters):
 		assert url is not None, 'Model parameters could not be located, either locally or using a specified url.'
+		# assert directory exists
+		directory = os.path.dirname(path_to_parameters)
+		if not os.path.isdir(directory):
+			os.makedirs(directory)
+		# download parameters using curl
+		# could change this to `pip install wget` if this doesn't work on all os
 		subprocess.run(shlex.split(f'curl {url} -L --output {path_to_parameters}'))
 	# load parameters
 	params: ExportedModel = torch.load(path_to_parameters, map_location='cpu')
 	# load model
 	m = Model(*params['model_args'].values(), **params['model_kwargs'])
 	m.load_state_dict(params['model_state_dict'])
-	return m
+	return m.eval()
