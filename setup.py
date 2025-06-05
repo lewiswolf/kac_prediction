784efa7f5d514fa5f67be3ce4ce4893d18c47969
lewiswolf <lewiswolstanholme@gmail.com>
2025-06-05 11:45:11 +0100

    update setup.py metadata

--- setup.py
+++ setup.py
@@ -54,6 +54,7 @@ setup(
 		'Programming Language :: Python :: 3 :: Only',
 		'Programming Language :: Python :: 3.11',
 		'Programming Language :: Python :: 3.12',
+		'Programming Language :: Python :: 3.13',
 		'Typing :: Typed',
 	],
 	description=short_description,
